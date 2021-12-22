3f32a26291606891217ef1abad1f80d46772e80b
Alexander Grooff <alexandergrooff@gmail.com>
2021-12-22 16:58:28 +0100

    feat: add --verbose flag

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from loguru import logger
 import crossplane
 from nginx_analysis.analysis import get_directive_values, get_unique_directives, parse_config
@@ -9,6 +10,11 @@ from nginx_analysis.input import get_args
 
 if __name__ == "__main__":
     args = get_args()
+
+    log_level = "DEBUG" if args.verbose else "INFO"
+    logger.remove()
+    logger.add(sys.stderr, level=log_level)
+
     root_config = parse_config(args.file)
     directives = get_unique_directives(root_config)
     logger.info(f"Found directives in config: {directives}")
--- nginx_analysis/input.py
+++ nginx_analysis/input.py
@@ -7,4 +7,5 @@ def get_args():
     parser = ArgumentParser()
     parser.add_argument("directive", help="Parse directive from configs")
     parser.add_argument("-f", "--file", help="Root nginx file", default="/etc/nginx/nginx.conf")
+    parser.add_argument("-v", "--verbose", help="Set verbosity level to debug", action="store_true")
     return parser.parse_args()
