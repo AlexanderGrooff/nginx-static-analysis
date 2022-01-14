6ca33f230b0ad189f8e63e147e1aeeaf389fdacf
Alexander Grooff <alexandergrooff@gmail.com>
2022-01-14 13:37:54 +0100

    tests: add test that should pass
    This doesn't quite pass yet but TDD and stuff

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -11,3 +11,19 @@ class TestIntegration(TestCase):
         root_config = self.get_example_root_conf()
         lines = get_directive_matches(root_config, directive_name="user")
         self.assertEqual(lines[0], expected_line)
+
+    def test_two_server_names_are_parsed(self):
+        root_config = self.get_example_root_conf()
+        lines = get_directive_matches(root_config, directive_name="server_name")
+        expected_lines = [
+            NginxLineConfig(
+                line=2,
+                file="/etc/nginx/servers/example.com.conf",
+                directive="server_name",
+                args=["example.com", "www.example.com"],
+            ),
+            NginxLineConfig(
+                line=1, file="/etc/nginx/nginx.conf", directive="user", args=["app"]
+            ),
+        ]
+        self.assertEqual(lines, expected_lines)
