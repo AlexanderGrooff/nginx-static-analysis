1ed5278bef4f0616cbe7d002da420d82fb9c0f2f
Alexander Grooff <alexandergrooff@gmail.com>
2022-07-20 09:04:42 +0200

    test: add logline_to_filters testcase

---
+++ tests/unit/log/test_logline_to_filters.py
@@ -0,0 +1,30 @@
+from nginx_analysis.dataclasses import AllFilter, DirectiveFilter
+from nginx_analysis.filter import logline_to_filters
+from tests.testcase import TestCase
+
+
+class TestLoglineToFilters(TestCase):
+    def test_server_name_is_set_as_filter(self):
+        parsed_line = {"server_name": "example.com"}
+        filters = logline_to_filters(parsed_line)
+        self.assertEqual(
+            filters,
+            AllFilter(
+                filters=[DirectiveFilter(directive="server_name", value="example.com")]
+            ),
+        )
+
+    def test_location_is_parsed_from_request(self):
+        parsed_line = {"request": "GET /foo/bar"}
+        filters = logline_to_filters(parsed_line)
+        self.assertEqual(
+            filters,
+            AllFilter(
+                filters=[DirectiveFilter(directive="location", value="/foo/bar")]
+            ),
+        )
+
+    def test_unknown_directive_is_ignored(self):
+        parsed_line = {"foo": "bar"}
+        filters = logline_to_filters(parsed_line)
+        self.assertEqual(filters, AllFilter(filters=[]))
