bd6a631ea3335b810fd76276989828e9fdc81fb6
Alexander Grooff <alexandergrooff@gmail.com>
2022-10-25 12:21:13 +0200

    fix: build changelog

--- build.sh
+++ build.sh
@@ -17,42 +17,18 @@ fi
 
 # Install dependencies
 sudo apt-get update && sudo apt-get install -y \
-  build-essential git-buildpackage debhelper devscripts dh-python dh-systemd debian-archive-keyring
+  build-essential git-buildpackage debhelper devscripts dh-python \
+  dh-systemd debian-archive-keyring quilt cowbuilder pristine-tar fakeroot
 
-gbp dch --debian-branch=main --commit --commit-msg=$MESSAGE --release --spawn-editor=never --ignore-branch
-gbp buildpackage --git-pbuilder --git-export-dir=$BUILDAREA-$DIST --git-dist=$DIST --git-arch=$ARCH \
---git-debian-branch=$TEMPBRANCH --git-ignore-new
-
-echo "Updating setup.py with version $VERSION"
-perl -pi -e 's/version="[^"]*",/version=\"$ENV{"VERSION"}\",/g;' setup.py
-
-echo "Adding setup.py to git index"
-git add setup.py
-
-echo "Committing setup.py version update"
-git commit setup.py -m "Update version in setup.py to $VERSION"
-
-echo "Generating changelog changelog"
-gbp dch --debian-tag="%(version)s" --new-version=$VERSION --debian-branch $BRANCH --release --commit
-
-echo "Building package for $DIST"
-
-git checkout $BRANCH
-TEMPBRANCH="$BRANCH-build-$DIST-$VERSION"
-git checkout -b $TEMPBRANCH
+if [ ! -z $CHANGELOG ]; then
+  gbp dch --debian-branch=main --commit --commit-msg=$MESSAGE --release --spawn-editor=never --ignore-branch
+fi
 
 mkdir -p $BUILDAREA-$DIST
-gbp buildpackage --git-pbuilder --git-export-dir=$BUILDAREA-$DIST --git-dist=$DIST --git-arch=$ARCH \
---git-debian-branch=$TEMPBRANCH --git-ignore-new
 
-git checkout $BRANCH
-git branch -D $TEMPBRANCH
+# Build base cow image
+DIST=$DIST ARCH=$ARCH git-pbuilder create
 
-echo
-echo "*************************************************************"
-echo
-
-echo "Creating tag $VERSION"
-git tag $VERSION
-
-echo "Now push the commit with the version update and the tag: git push; git push --tags"
+gbp buildpackage --git-pbuilder --git-export-dir=$BUILDAREA-$DIST \
+--git-dist=$DIST --git-arch=$ARCH \
+--git-ignore-new --git-ignore-branch
