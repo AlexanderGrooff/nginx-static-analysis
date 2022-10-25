28b291eb9217930fca9bb1a9fb9aae89ed8860ef
Alexander Grooff <alexandergrooff@gmail.com>
2022-10-25 11:47:00 +0200

    feat: build package during test

--- build.sh
+++ build.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 set -e
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
 
 ARCH="amd64"
 DIST="${DIST:-buster}"
 BUILDAREA="/tmp/nsa-build"
 BRANCH="main"
+MESSAGE="chore: release version $VERSION"
 
-if [ -z $VERSION ];
+if [ -z $VERSION ]; then
     echo "Specify a version first"
     exit 2
 fi
 
-if [ "$(git rev-parse --abbrev-ref HEAD)" != $BRANCH ]; then
-    echo "You are not on the $BRANCH branch, aborting"
-    /bin/false
-fi;
+# Install dependencies
+sudo apt-get update && sudo apt-get install -y \
+  build-essential git-buildpackage debhelper devscripts dh-python dh-systemd debian-archive-keyring
+
+gbp dch --debian-branch=main --commit --commit-msg=$MESSAGE --release --spawn-editor=never --ignore-branch
+gbp buildpackage --git-pbuilder --git-export-dir=$BUILDAREA-$DIST --git-dist=$DIST --git-arch=$ARCH \
+--git-debian-branch=$TEMPBRANCH --git-ignore-new
 
 echo "Updating setup.py with version $VERSION"
 perl -pi -e 's/version="[^"]*",/version=\"$ENV{"VERSION"}\",/g;' setup.py
