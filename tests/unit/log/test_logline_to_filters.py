1c052ce233e150098ea4fe05a9ad3fddbc519378
Alexander Grooff <alexandergrooff@gmail.com>
2022-10-28 00:51:26 +0200

    fix: combine logline filters into anyfilter
    Create an AllFilter per logline and combine those into an AnyFilter to
    show directives matching _any_ of the loglines, not _all_ of the
    loglines.

--- tests/unit/log/test_logline_to_filters.py
+++ tests/unit/log/test_logline_to_filters.py
@@ -1,12 +1,12 @@
 from nginx_analysis.dataclasses import AllFilter, DirectiveFilter
-from nginx_analysis.filter import logline_to_filters
+from nginx_analysis.filter import logline_to_filter
 from tests.testcase import TestCase
 
 
 class TestLoglineToFilters(TestCase):
     def test_server_name_is_set_as_filter(self):
         parsed_line = {"server_name": "example.com"}
-        filters = logline_to_filters(parsed_line)
+        filters = logline_to_filter(parsed_line)
         self.assertEqual(
             filters,
             AllFilter(
@@ -16,7 +16,7 @@ class TestLoglineToFilters(TestCase):
 
     def test_location_is_parsed_from_request(self):
         parsed_line = {"request": "GET /foo/bar"}
-        filters = logline_to_filters(parsed_line)
+        filters = logline_to_filter(parsed_line)
         self.assertEqual(
             filters,
             AllFilter(
@@ -26,5 +26,5 @@ class TestLoglineToFilters(TestCase):
 
     def test_unknown_directive_is_ignored(self):
         parsed_line = {"foo": "bar"}
-        filters = logline_to_filters(parsed_line)
+        filters = logline_to_filter(parsed_line)
         self.assertEqual(filters, AllFilter(filters=[]))
