4439005eed1c6a35890e3d9323241e21d284f86f
Alexander Grooff <alexandergrooff@gmail.com>
2023-05-17 00:08:06 +0200

    feat: arch packaging

--- scripts/verify_setup.sh
+++ scripts/verify_setup.sh
@@ -3,10 +3,12 @@
 set -x
 set -e
 
-# Verify setup.py version matches the git tag
+# Verify versions matches the git tag
 SETUP_VERSION=$(grep -oP '(?<=version=")[^"]+' setup.py)
+PKGBUILD_VERSION=$(grep -oP '(?<=pkgver=)[^ ]+' PKGBUILD)
 GIT_TAG=$(git describe --tags --abbrev=0)
 test "$SETUP_VERSION" == "$GIT_TAG" || exit 1
+test "$PKGBUILD_VERSION" == "$GIT_TAG" || exit 1
 
 # Verify setup.py requirements match requirements/base.txt
 SETUP_REQUIREMENTS=$(sed -n -e '/requirements = """/,/"""/ p' setup.py | grep -v '"""' | grep -v '^$')
