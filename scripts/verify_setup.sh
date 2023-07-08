571d46f669b7f49c6354ae59992ecb68eedf475f
Alexander Grooff <alexandergrooff@gmail.com>
2023-07-08 10:27:33 +0200

    fix: ensure verify setup in pre-commit

--- scripts/verify_setup.sh
+++ scripts/verify_setup.sh
@@ -6,7 +6,7 @@ set -e
 # Verify versions matches the git tag
 SETUP_VERSION=$(grep -oP '(?<=version=")[^"]+' setup.py)
 PKGBUILD_VERSION=$(grep -oP '(?<=pkgver=)[^ ]+' PKGBUILD)
-PYPROJECT_VERSION=$(grep -oP '(?<=version = )[^ ]+' pyproject.toml)
+PYPROJECT_VERSION=$(grep -oP '(?<=version = ")[^"]+' pyproject.toml)
 GIT_TAG=$(git describe --tags --abbrev=0)
 test "$SETUP_VERSION" == "$GIT_TAG" || (echo "setup.py is version ${SETUP_VERSION} but should be ${GIT_TAG}" && exit 1)
 test "$PKGBUILD_VERSION" == "$GIT_TAG" || (echo "PKGBUILD is version ${PKGBUILD_VERSION} but should be ${GIT_TAG}" && exit 1)
